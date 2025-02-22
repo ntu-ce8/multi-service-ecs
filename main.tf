89fa44c0f16c8ece9f97785d57b64ae66a740631
Jesstern Rays <jsstrn@users.noreply.github.com>
2025-02-22 12:10:14 +0800

    Move data calls to variables.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-locals {
-  prefix = "jsstrn"
-}
-
-data "aws_caller_identity" "current" {}
-
-data "aws_region" "current" {}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "public" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
 module "ecs" {
   source  = "terraform-aws-modules/ecs/aws"
   version = "~> 5.9.0"
@@ -33,14 +14,14 @@ module "ecs" {
 
   services = {
     jsstrn-service-flask-s3 = {
-      cpu    = 512
-      memory = 1024
+      cpu                                = 512
+      memory                             = 1024
       assign_public_ip                   = true
       deployment_minimum_healthy_percent = 100
       subnet_ids                         = flatten(data.aws_subnets.public.ids)
       security_group_ids                 = [module.ecs_sg.security_group_id]
       container_definitions = {
-        jsstrn-container-flask-s3 = { 
+        jsstrn-container-flask-s3 = {
           essential = true
           image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${local.prefix}-ecr:latest"
           port_mappings = [
@@ -50,11 +31,11 @@ module "ecs" {
             }
           ]
         }
-      }      
+      }
     }
     jsstrn-service-flask-sqs = {
-      cpu    = 512
-      memory = 1024
+      cpu                                = 512
+      memory                             = 1024
       assign_public_ip                   = true
       deployment_minimum_healthy_percent = 100
       subnet_ids                         = flatten(data.aws_subnets.public.ids)
@@ -70,7 +51,7 @@ module "ecs" {
             }
           ]
         }
-      }      
+      }
     }
   }
 }
@@ -84,6 +65,21 @@ module "ecs_sg" {
   vpc_id      = data.aws_vpc.default.id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-5001-tcp", "http-5002-tcp"]
-  egress_rules        = ["all-all"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 5001
+      to_port     = 5001
+      protocol    = "tcp"
+      description = "Service for Flask S3"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 5002
+      to_port     = 5002
+      protocol    = "tcp"
+      description = "Service for Flask SQS"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+  egress_rules = ["all-all"]
 }
